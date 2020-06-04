306d16db9eec81e4eec8beff56dd62d2d6780272
yacksebblen <johnjacksebben@gmail.com>
2020-06-04 12:54:08 -0400

    Basic movement added

---
+++ Zombie Tower/Assets/Scripts/Motion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Jackseb.Zombie
+{
+	public class Motion : MonoBehaviour
+	{
+		public float speed;
+
+		private Rigidbody rig;
+
+		private void Start()
+		{
+			rig = GetComponent<Rigidbody>();
+		}
+
+		void FixedUpdate()
+		{
+			float _hMove = Input.GetAxis("Horizontal");
+			float _vMove = Input.GetAxis("Vertical");
+
+			Vector3 _direction = new Vector3(_hMove, 0, _vMove);
+			_direction.Normalize();
+
+			rig.velocity = transform.TransformDirection(_direction) * speed * Time.deltaTime;
+		}
+	}
+}
