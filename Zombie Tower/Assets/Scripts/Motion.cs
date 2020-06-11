7d770fce90f1fecb11a004cda4b2f2f18d4bc9a5
yacksebblen <johnjacksebben@gmail.com>
2020-06-11 00:02:56 -0400

    Added running and jumping

--- Zombie Tower/Assets/Scripts/Motion.cs
+++ Zombie Tower/Assets/Scripts/Motion.cs
@@ -7,6 +7,11 @@ namespace Com.Jackseb.Zombie
 	public class Motion : MonoBehaviour
 	{
 		public float speed;
+		public float walkModifier;
+		public float jumpForce;
+		//public Camera normalCam;
+		public Transform groundDetect;
+		public LayerMask ground;
 
 		private Rigidbody rig;
 
@@ -18,13 +23,35 @@ namespace Com.Jackseb.Zombie
 
 		void FixedUpdate()
 		{
+			// Axes
 			float _hMove = Input.GetAxisRaw("Horizontal");
 			float _vMove = Input.GetAxisRaw("Vertical");
 
+			// Controls
+			bool walk = Input.GetKey(KeyCode.LeftShift);
+			bool jump = Input.GetKeyDown(KeyCode.Space);
+
+			// States
+			bool isGrounded = Physics.Raycast(groundDetect.position, Vector3.down, 0.1f, ground);
+			bool isJumping = jump && isGrounded;
+			bool isWalking = walk && !isJumping && isGrounded;
+
+			// Jumping
+			if (isJumping)
+			{
+				rig.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+			}
+			
+			// Movement
 			Vector3 _direction = new Vector3(_hMove, 0, _vMove);
 			_direction.Normalize();
 
-			rig.velocity = transform.TransformDirection(_direction) * speed * Time.deltaTime;
+			float _adjustedSpeed = speed;
+			if (isWalking) _adjustedSpeed *= walkModifier;
+
+			Vector3 _targetVelocity = transform.TransformDirection(_direction) * _adjustedSpeed * Time.deltaTime;
+			_targetVelocity.y = rig.velocity.y;
+			rig.velocity = _targetVelocity;
 		}
 	}
 }
